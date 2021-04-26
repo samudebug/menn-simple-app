8529fee588ee40914703b832615e3c80bf262266
Samuel Martins <samuel.ubuntu@gmail.com>
2021-04-26 17:24:09 -0300

    Fixing firebase import

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,7 +1,7 @@
-const admin = require("firebase-admin");
+import * as admin from 'firebase-admin';
 const credFile = require("./firebase_config.json");
 
-if (admin.apps === 0) {
+if (admin.apps.length === 0) {
     admin.initializeApp({
         credential: admin.credential.cert(credFile)
     });
