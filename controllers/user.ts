2c4e49ca8dd75586f8b9b2613a7aba257e3a48c7
Samuel Martins <samuel.ubuntu@gmail.com>
2021-04-26 16:18:00 -0300

    Showing download link for app

--- controllers/user.ts
+++ controllers/user.ts
@@ -18,12 +18,14 @@ export const getUserByName = async (name: string) => {
 export const createLinkForUser = async (name: string) => {
     const firebaseApiKey = process.env.FIREBASE_API_KEY;
     const basePath = "https://menn-simple-app-samudebug.vercel.app";
+    const link = `${basePath}/${name}`;
     const response = await axios.post(`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${firebaseApiKey}`, {
             dynamicLinkInfo: {
                 domainUriPrefix: "https://mennlinks.page.link",
-                link: `${basePath}/${name}`,
+                link: link,
                 androidInfo: {
-                    androidPackageName: "com.mennmobileapp"
+                    androidPackageName: "com.mennmobileapp",
+                    androidFallbackLink: link
                 },
             }
     });
--- pages/[name]/index.tsx
+++ pages/[name]/index.tsx
@@ -1,7 +1,7 @@
 import { Container, Button } from 'react-bootstrap'
 import { GetServerSideProps } from 'next'
 import { getUserByName } from '../../controllers/user'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -17,6 +17,20 @@ export default function UserPage({userData}) {
     const [link, setLink] = useState("");
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
+    const [showDownloadLink, setShowDownloadLink] = useState(false);
+    const [downloadLink, setDownloadLink] = useState("")
+    useEffect(() => {
+        const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+
+        const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+        if (isAndroid) {
+            setDownloadLink("https://play.google.com/store/apps/details?id=com.instagram.android")
+            setShowDownloadLink(true)
+        } else if (isAndroid) {
+            setDownloadLink("https://apps.apple.com/br/app/instagram/id389801252")
+            setShowDownloadLink(true)
+        }
+    })
     const createShareLink = async () => {
         setSuccess(false);
         setError(false);
@@ -35,7 +49,12 @@ export default function UserPage({userData}) {
             <h2>This page is for {userData.name}</h2>
             <Button variant="primary" onClick={createShareLink}>Share</Button>
 
-            {success ? (<h4>Use this link to open in the app: <a href={link}>{link}</a></h4>) : (<div></div>)}
+            {success ? (
+            <div>
+                <h4>Use this link to open in the app: <a href={link}>{link}</a></h4>
+                {showDownloadLink ? (<a href={downloadLink}><Button variant="primary">Download the App</Button></a>) : (<div></div>)}
+            </div>
+            ) : (<div></div>)}
             {error ? (<h4>An error has ocurred when generating the link</h4> ): (<div></div>)}
         </Container>
     );
